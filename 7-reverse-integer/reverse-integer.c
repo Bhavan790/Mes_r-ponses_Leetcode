b3fb7db14be92318f91c4767361d1152b06ac2db
Bhavan kumar <230901014@rajalakshmi.edu.in>
2026-03-26 16:18:41 +0530

    Time: 3 ms (54.57%) | Memory: 9.4 MB (23.70%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,15 @@
+
+int reverse(int x)
+{
+  long long a,b=0;
+  while(x!=0)
+  {
+    a=x%10;
+    b=b*10+a;
+    x=x/10;
+  }
+  if(b < -2147483648 || b > 2147483647){
+    return 0;
+  }
+  return b;
+}
