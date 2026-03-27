cbbc0588767d05c0e8cd743fcb587b92fe5448f2
Bhavan kumar <230901014@rajalakshmi.edu.in>
2026-03-27 11:57:11 +0530

    Time: 11 ms (13.53%) | Memory: 19.3 MB (66.47%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
@@ -0,0 +1,14 @@
+class Solution:
+    def areSimilar(self, mat: List[List[int]], k: int) -> bool:
+        res=deepcopy(mat)
+        # s=0
+        for _ in range(k) :
+            # print(s)
+            for i in range(len(res)) :
+                if i%2==0 :
+                    res[i].append(res[i].pop(0))
+                else :
+                    res[i].insert(0,res[i].pop())
+            # s+=1
+            # print(res)
+        return mat==res
