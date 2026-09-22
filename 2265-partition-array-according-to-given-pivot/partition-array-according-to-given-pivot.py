72d6a4cdc0033a6a42094428bebce5882bfd2e09
Bhavan kumar <bhavank657@gmail.com>
2026-09-22 19:02:51 +0530

    Time: 23 ms (83.24%) | Memory: 33.6 MB (80.21%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,13 +1,16 @@
 class Solution:
     def pivotArray(self, nums: list[int], pivot: int) -> list[int]:
-        res=[]
-        for i in nums :
-            if i<pivot :
-                res.append(i)
-        for i in nums :
-            if i==pivot :
-                res.append(i)
-        for i in nums :
-            if i>pivot :
-                res.append(i)
-        return res
+        less = []
+        equal = []
+        greater = []
+        
+        for i in nums:
+            if i < pivot:
+                less.append(i)
+            elif i == pivot:
+                equal.append(i)
+            else:
+                greater.append(i)
+                
+        return less + equal + greater
+# AI
